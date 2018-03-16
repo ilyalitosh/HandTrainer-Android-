1d93cd4d6fe4a2f2703cafbc34e463bbd1415457
ilya_litosh <ilya_litosh@hotmail.com>
2018-03-16 17:48:42 +0300

    тестовый коммит

---
+++ app/src/main/java/com/litosh/ilya/handtrainer/MainActivity.java
@@ -0,0 +1,15 @@
+package com.litosh.ilya.handtrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/litosh/ilya/handtrainer/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.litosh.ilya.handtrainer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ilya_ on 16.03.2018.
+ */
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+
+    }
+}
